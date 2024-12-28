b6a2d5107a8803535707789969ab0c5171dd4ea4
MALYKAHUMAIRA <vivomalika81@gmail.com>
2024-12-28 17:15:45 +0700

    Initial commit

---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,37 @@
+ <section class="slider_section">
+     <div class="slider_container">
+         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+             <div class="carousel-inner">
+                 <div class="carousel-item active">
+                     <div class="container-fluid">
+                         <div class="row">
+                             <div class="col-md-7">
+                                 <div class="detail-box">
+                                     <h1>
+                                         Selamat Datang di <br>
+                                         Gift Shop
+                                     </h1>
+                                     <p>
+                                         Selamat datang di Anvengers Costum!
+                                         Kami adalah toko online yang menghadirkan koleksi pakaian trendi dan
+                                         berkualitas untuk memenuhi kebutuhan gaya Anda.Membuat anda menjadi superhero.
+                                     </p>
+                                     <a href="">
+                                         Hubungi Kami
+                                     </a>
+                                 </div>
+                             </div>
+                             <div class="col-md-5 ">
+                                 <div class="img-box">
+                                     <img style="width:600px" src="images/image3.jpeg" alt="" />
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+
+             </div>
+
+         </div>
+     </div>
+ </section>
